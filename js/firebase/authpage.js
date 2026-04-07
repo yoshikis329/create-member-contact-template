c89581fa80ddc06496161d61f1774f00a25f204b
yoshikis <yoshikis@lycorp.co.jp>
2026-04-08 05:22:18 +0900

    firebaseの設定を別ファイルに

--- js/firebase/authpage.js
+++ js/firebase/authpage.js
@@ -1,5 +1,5 @@
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { auth } from "./common.js";
+import { auth } from "./firebase-config.js";
 // ログイン状態の監視
 onAuthStateChanged(auth, (user) => {
     if (!user) {
